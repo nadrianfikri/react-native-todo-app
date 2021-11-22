7afd9091740c7b165aada4fb3de75d57d5848a39
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-11-22 23:35:09 +0700

    setup icon and splash-screen for building apk

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import Container from './Container';
-import Home from './src/screen/Home';
 
 export default function App() {
   return (
@@ -13,9 +11,3 @@ export default function App() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
